9f152de22ef9ba7222c0cf5c74036e2baa64a4ad
hui <huynguyeexn@gmail.com>
2021-10-23 21:28:38 +0700

    Update new api room seats

--- src/app/api/seatsApi.ts
+++ src/app/api/seatsApi.ts
@@ -1,9 +1,9 @@
 import { ListResponse } from './../interfaces/common';
-import { ISeat } from './../interfaces/seat';
+import { Seat } from './../interfaces/seat';
 import axiosClient from './axiosClient';
 
 const seatApi = {
-	getSeatOfRoom(roomId?: string | number): Promise<ListResponse<ISeat>> {
+	getSeatOfRoom(roomId?: string | number): Promise<ListResponse<Seat>> {
 		const url = `/rooms/${roomId}/seats`;
 		return axiosClient.get(url);
 	},
--- src/app/features/room/components/RoomShowcase.tsx
+++ src/app/features/room/components/RoomShowcase.tsx
@@ -1,54 +1,46 @@
-import React from 'react';
+import screenImg from 'app/assets/images/screen.png';
+import { Room } from 'app/interfaces';
 import { makeRows } from 'app/utils/helper';
+import React from 'react';
 
 interface Props {
 	rows: number;
 	cols: number;
-	seats?: number[][];
+	room?: Room;
 	onSelectSeat?: (row: number, col: number) => void;
 }
 
-const RoomShowcase = ({ rows, cols, seats, onSelectSeat }: Props) => {
-	console.log(`seats`, seats);
-	if (seats) {
-		rows = seats.length;
-		cols = seats[0].length;
-	}
-
+const RoomShowcase = ({ rows, cols, room, onSelectSeat }: Props) => {
 	const rowsAlpha: Array<string> = makeRows(rows);
-
-	const seatStatus = (status: number) => {
-		switch (status) {
-			case 1:
-				return 'occupied';
-			case 2:
-				return 'selected';
-
-			default:
-				return '';
-		}
-	};
 
 	return (
 		<div className="room">
-			<div className="screen"></div>
+			<div className="screen">
+				<img src={screenImg} alt="màn hình" />
+			</div>
 			<div className="seats" style={{ gridTemplateColumns: `repeat(${cols}, min-content)` }}>
-				{rowsAlpha.map((row, rowIdx) => {
-					let rowEl = [];
-					for (let colIndex = 0; colIndex < cols; colIndex++) {
-						rowEl.push(
-							<div
-								key={`seat-${row}-${colIndex + 1}`}
-								className={`seat ${seatStatus(seats ? seats[rowIdx][colIndex] : 0)}`}
-								onClick={() => (onSelectSeat ? onSelectSeat(rowIdx, colIndex) : {})}
-							>
-								{row}
-								{colIndex + 1}
+				{room
+					? room.seats.map((seat, colIdx) => (
+							<div key={`seat-${seat.index}`} className={`seat`}>
+								{seat.label}
 							</div>
-						);
-					}
-					return rowEl;
-				})}
+					  ))
+					: rowsAlpha.map((row, rowIdx) => {
+							let rowEl = [];
+							for (let colIndex = 0; colIndex < cols; colIndex++) {
+								rowEl.push(
+									<div
+										key={`seat-${row}-${colIndex + 1}`}
+										className={`seat`}
+										onClick={() => (onSelectSeat ? onSelectSeat(rowIdx, colIndex) : {})}
+									>
+										{row}
+										{colIndex + 1}
+									</div>
+								);
+							}
+							return rowEl;
+					  })}
 			</div>
 		</div>
 	);
